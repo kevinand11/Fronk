e63c085756bac55d51098b740c1180b170f31f7f
kevinand11 <kevinfizu@gmail.com>
2023-05-01 17:27:41 +0100

    tab 1 text

--- src/components/features/Tab1.vue
+++ src/components/features/Tab1.vue
@@ -4,5 +4,6 @@
 		Our platform allows you to feed low-quality memes to the ai and receive a higher quality meme in return.
 		We are building a social network where people can create and share memes with a copyright option to ensure proper attribution.
 		In addition, we are developing fast-paced multiplayer card games that operates on a win2earn and free2play model, aiming for real sustainability in the web3 gaming space.
+		We would also be integrating an AI feature as well as an NFT Marketplace
 	</div>
 </template>
