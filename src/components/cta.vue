789b37e659861b3f94a152c08adfb87408add159
kevinand11 <kevinfizu@gmail.com>
2023-05-02 13:11:08 +0100

    telegram link

--- src/components/cta.vue
+++ src/components/cta.vue
@@ -2,11 +2,12 @@
 	<section class="px-4 container mx-auto flex flex-col items-center justify-center mt-12 pb-8">
 		<img src="/images/cta.svg" alt="hero image" class="md:w-[50%]">
 
-		<nuxt-link
-			to="#"
+		<a
+			target="_blank"
+			href="https://t.me/fronk_ai"
 			class="px-4 w-[210px] h-[59px] flex justify-center items-center rounded-2xl !bg-white md:mt-20 mt-10 font-medium text-lg"
 		>
 			Join Telegram
-		</nuxt-link>
+		</a>
 	</section>
 </template>
