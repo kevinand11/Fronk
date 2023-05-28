392b7bb55bc27cabf9eea354429be4fe8c3baf2b
kevinand11 <kevinfizu@gmail.com>
2023-05-28 19:40:16 +0100

    changes

--- src/components/features/Tab1.vue
+++ src/components/features/Tab1.vue
@@ -5,5 +5,14 @@
 		We are building a social network where people can create and share memes with a copyright option to ensure proper attribution.
 		In addition, we are developing fast-paced multiplayer card games that operates on a win2earn and free2play model, aiming for real sustainability in the web3 gaming space.
 		We would also be integrating an AI feature for a better experience.
+
+		Enter the world of <span class="highlight">FRONKAI</span>. Where you earn $USDT as you hold our native token $FRONK (Great for jeets i would say),
+		Have a personal buddy you can ask anything you want.
+		Also our platform is a social network where users can create and share memes.
+		We've implemented a copyright feature to ensure proper attribution.
+		Additionally, we're developing more exciting utilities in the future one of them making betting and gaming alot more exciting!
+		Keep reading to understand more about our ecosystem!
+		<br>
+		LET'S FRONK IT UP!
 	</div>
 </template>
