4e01fe08136e63389c0b84fede97aa0f98aac890
kromate <akpananthony33@gmail.com>
2023-05-29 20:08:11 +0100

    fix: removed the first paragraph from tab 1

--- src/components/features/Tab1.vue
+++ src/components/features/Tab1.vue
@@ -1,10 +1,10 @@
 <template>
 	<div>
-		<span class="highlight">Fronk Ai</span> is the first AI-powered GameFi and meme creator.
+		<!-- <span class="highlight">Fronk Ai</span> is the first AI-powered GameFi and meme creator.
 		Our platform allows you to feed low-quality memes to the ai and receive a higher quality meme in return.
 		We are building a social network where people can create and share memes with a copyright option to ensure proper attribution.
 		In addition, we are developing fast-paced multiplayer card games that operates on a win2earn and free2play model, aiming for real sustainability in the web3 gaming space.
-		We would also be integrating an AI feature for a better experience.
+		We would also be integrating an AI feature for a better experience. -->
 
 		Enter the world of <span class="highlight">FRONKAI</span>. Where you earn $USDT as you hold our native token $FRONK (Great for jeets i would say),
 		Have a personal buddy you can ask anything you want.
