4aa01072ae6f7481428295705d8edb39915cfd1b
Izuchukwu Kevin I <Kevinfizu@gmail.com>
2023-05-30 21:27:51 +0100

    Update Tab1.vue

--- src/components/features/Tab1.vue
+++ src/components/features/Tab1.vue
@@ -1,17 +1,8 @@
 <template>
 	<div>
-		<!-- <span class="highlight">Fronk Ai</span> is the first AI-powered GameFi and meme creator.
-		Our platform allows you to feed low-quality memes to the ai and receive a higher quality meme in return.
-		We are building a social network where people can create and share memes with a copyright option to ensure proper attribution.
-		In addition, we are developing fast-paced multiplayer card games that operates on a win2earn and free2play model, aiming for real sustainability in the web3 gaming space.
-		We would also be integrating an AI feature for a better experience. -->
-
-		Enter the world of <span class="highlight">FRONKAI</span>. Where you earn $USDT as you hold our native token $FRONK (Great for jeets i would say),
-		Have a personal buddy you can ask anything you want.
-		Also our platform is a social network where users can create and share memes.
-		We've implemented a copyright feature to ensure proper attribution.
-		Additionally, we're developing more exciting utilities in the future one of them making betting and gaming alot more exciting!
-		Keep reading to understand more about our ecosystem!
+		Enter the world of <span class="highlight">FRONK AI!</span> Where Inspiration from PEPE meets AI!
+		We aim to build fun, fair and robust tokens for Frog and Crypto lovers!
+		Packed wuth several utilities, partnerships and long term plans - This is one Funky ride you wouldn't wanna miss.
 		<br>
 		LET'S FRONK IT UP!
 	</div>
