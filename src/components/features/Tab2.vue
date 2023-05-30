1ca02cc04b0ecda8655265e1cd527f5f72efc6e3
Izuchukwu Kevin I <Kevinfizu@gmail.com>
2023-05-30 21:32:23 +0100

    Update Tab2.vue

--- src/components/features/Tab2.vue
+++ src/components/features/Tab2.vue
@@ -1,10 +1,7 @@
 <template>
 	<div>
-		<span class="highlight">FRONK AI</span> will have a <span class="highlight">USDT</span> reflection feature which will encourage more diamond hands in our ecosystem which will at the same time promote market stability while offering investors a chance to earn some <span class="highlight">$USDT</span> on all transactions being made on the protocol.<br>
-		<span class="highlight">FRONK Ai</span> has utilized a built in and fixed reward mechanism that allocates a percentage <span class="highlight">(3% USDT REWARDS)</span> of the transaction fees trading <span class="highlight">$FRONK</span> to be redistributed among token holders.<br>
-		So in essence this allows reflection token holders to earn additional <span class="highlight">$USDT</span> in proportion to the number of tokens they own as compared to the total supply of <span class="highlight">$FRONK</span>.<br>
-		And now everything is pumping, this is a great time to stack up and hold unto your gems!<br>
-		At <span class="highlight">$FRONK</span> we would be rewarding buyers 3% <span class="highlight">$USDT</span> reflection from transaction tax taken from every buy/sell with <span class="highlight">$FRONK</span> to holders in the form of <span class="highlight">$USDT</span>.<br>
-		We would also have a dashboard for holders to view their USDT earned for easy tracking as well.
+		"Ask Fronkie" A fun friendly AI build on Twitter to add the fun to your Twitter experience!
+		Tag Fronkie to Tweets to get explanations on wtf is going on!
+		Currently on BETA stage and ready to be launched to the public upon <span class="highlight">$FRONK</span> launch!
 	</div>
 </template>
