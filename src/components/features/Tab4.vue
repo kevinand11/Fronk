1b22f35d81ef370225863992e10c24f72f36b696
kevinand11 <kevinfizu@gmail.com>
2023-05-30 22:51:25 +0100

    Changes

--- src/components/features/Tab4.vue
+++ src/components/features/Tab4.vue
@@ -1,12 +1,11 @@
 <template>
 	<div>
 		This is the official <span class="highlight">$FRONK</span> Contract Address:<br>
-    0x4E823d2445bBE8d3979b6125e72c64dc04869D3D<br>
+		0x4E823d2445bBE8d3979b6125e72c64dc04869D3D<br>
 
-    You’ll be able to get your “Fronk” on right after <span class="highlight">$FRONK</span> Presale and Launch!<br>
+		You’ll be able to get your “Fronk” on right after <span class="highlight">$FRONK</span> Presale and Launch!<br>
 
-    After launch, You will be able to Buy <span class="highlight">$FRONK</span> directly on PancakeSwap 
-    with Slippage set  at 6% - 7%  due to our stated 6% BUY/SELL Tax used to fun Rewards, Marketing and overall development of <span class="highlight">$FRONK</span>
-    
+		After launch, You will be able to Buy <span class="highlight">$FRONK</span> directly on PancakeSwap 
+		with Slippage set  at 6% - 7%  due to our stated 6% BUY/SELL Tax used to fun Rewards, Marketing and overall development of <span class="highlight">$FRONK</span>
 	</div>
 </template>
