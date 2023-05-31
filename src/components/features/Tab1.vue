b8dea1c7d1e4fa8348619bb5a0baddaa6a507b39
Izuchukwu Kevin I <Kevinfizu@gmail.com>
2023-05-31 01:30:29 +0100

    Update Tab1.vue

--- src/components/features/Tab1.vue
+++ src/components/features/Tab1.vue
@@ -2,7 +2,7 @@
 	<div>
 		Enter the world of <span class="highlight">FRONK AI!</span> Where Inspiration from PEPE meets AI!
 		We aim to build fun, fair and robust tokens for Frog and Crypto lovers!
-		Packed wuth several utilities, partnerships and long term plans - This is one Funky ride you wouldn't wanna miss.
+		Packed with several utilities, partnerships and long term plans - This is one Funky ride you wouldn't wanna miss.
 		<br>
 		LET'S FRONK IT UP!
 	</div>
