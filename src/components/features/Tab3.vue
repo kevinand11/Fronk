bdcc0d2450eb561e0fddb6005042641b38ac6310
Izuchukwu Kevin I <Kevinfizu@gmail.com>
2023-05-31 01:10:18 +0100

    Update Tab3.vue

--- src/components/features/Tab3.vue
+++ src/components/features/Tab3.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="flex flex-col gap-4">
 		<p>
-			Get Airdropped wuth Fronky $USDT rewards for being less of a jeet and more of a real one!
-			A part of the Buy/Sell Tax is dedicateede to airdropping USDT rewards to <span class="highlight">$FRONK</span> holders...
+			Get Airdropped $USDT rewards for being less of a jeet!
+			A part of the Buy/Sell Tax is dedicated to airdropping USDT rewards to <span class="highlight">$FRONK</span> holders...
 			ALL DAY, EVERYDAY!
 		</p>
 		<img src="@/assets/images/wallet.svg" class="hidden mx-auto w-2/3" alt="">
