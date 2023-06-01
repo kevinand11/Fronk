3d1f96838304e706e539e5d71fe08940f9e77c28
kevin <kevinfizu@gmail.com>
2023-06-01 09:03:57 +0100

    change

--- src/components/features/Tab1.vue
+++ src/components/features/Tab1.vue
@@ -1,8 +1,8 @@
 <template>
 	<div>
 		Enter the world of <span class="highlight">FRONK AI!</span> Where Inspiration from PEPE meets AI!
-		We aim to build fun, fair and robust tokens for Frog and Crypto lovers!
-		Packed with several utilities, partnerships and long term plans - This is one Fronky ride you wouldn't wanna miss.
+		We aim to build a Fun, Fair and Robust token for Frog and Crypto lovers!
+		Packed with several utilities, partnerships and long term plans - This is one Fronky ride you would  lie about your height to get on.
 		<br>
 		LET'S FRONK IT UP!
 	</div>
