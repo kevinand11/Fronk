56c04f9f9312aec3489b65fab992691a4c6de155
kevin <kevinfizu@gmail.com>
2023-06-01 13:50:22 +0100

    gh-pages

--- src/components/features/Tab2.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div>
-		"Ask Fronkie" A fun friendly AI build on Twitter to add the fun to your Twitter experience!
-		Tag Fronkie to Tweets to get explanations on wtf is going on!
-		Currently on BETA stage and ready to be launched to the public upon <span class="highlight">$FRONK</span> launch!
-	</div>
-</template>
