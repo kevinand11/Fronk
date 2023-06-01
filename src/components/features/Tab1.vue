cffeb1706daefeab5790b690d321ca8323b10784
kevin <kevinfizu@gmail.com>
2023-06-01 09:10:58 +0100

    change

--- src/components/features/Tab1.vue
+++ src/components/features/Tab1.vue
@@ -2,7 +2,7 @@
 	<div>
 		Enter the world of <span class="highlight">FRONK AI!</span> Where Inspiration from PEPE meets AI!
 		We aim to build a Fun, Fair and Robust token for Frog and Crypto lovers!
-		Packed with several utilities, partnerships and long term plans - This is one Fronky ride you would  lie about your height to get on😉.
+		Packed with several utilities, partnerships and long term plans - This is one Fronky ride you would lie about your height to get on😉.
 		<br>
 		LET'S FRONK IT UP!
 	</div>
